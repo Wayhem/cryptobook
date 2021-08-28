f58f7c44303839ee77d48545b8edfbbf90dfc253
Jose Villarroel <oscarclarke9900@gmail.com>
2021-08-28 13:36:43 +0200

    Init: initial commit

---
+++ src/Components/System/Main.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const App = (): JSX.Element => <>hello</>
+
+export default App
---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom'
+import App from 'Components/System/Main'
+
+ReactDOM.render(
+  <>
+    <App />
+  </>,
+  document.getElementById('root')
+)
