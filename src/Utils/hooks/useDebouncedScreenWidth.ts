b93d231667693311e27eacda36efef06ad6d06a6
Jose Villarroel <oscarclarke9900@gmail.com>
2021-09-03 22:41:18 +0200

    Added screen width for mobile devices

--- src/Utils/hooks/useDebouncedScreenWidth.ts
+++ src/Utils/hooks/useDebouncedScreenWidth.ts
@@ -2,10 +2,10 @@ import { useEffect, useState } from 'react'
 import debounce from 'lodash.debounce'
 
 function useWindowWidth(delay = 500) {
-  const [width, setWidth] = useState(window.innerWidth)
+  const [width, setWidth] = useState(window.innerWidth || window.screen.width)
 
   useEffect(() => {
-    const handleResize = () => setWidth(window.innerWidth)
+    const handleResize = () => setWidth(window.innerWidth || window.screen.width)
     const debouncedHandleResize = debounce(handleResize, delay)
     window.addEventListener('resize', debouncedHandleResize)
     return () => {
